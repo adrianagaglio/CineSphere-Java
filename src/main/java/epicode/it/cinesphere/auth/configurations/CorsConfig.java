405923c78fe3385035cf40c945059028803df7e9
Adriana Gaglio <gaglioadriana25@gmail.com>
2025-03-31 18:06:32 +0200

    edit cors config

--- src/main/java/epicode/it/cinesphere/auth/configurations/CorsConfig.java
+++ src/main/java/epicode/it/cinesphere/auth/configurations/CorsConfig.java
@@ -10,10 +10,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true)
+                .allowCredentials(false)
                 .maxAge(3600);
     }
 
--- src/main/java/epicode/it/cinesphere/auth/configurations/SecurityConfig.java
+++ src/main/java/epicode/it/cinesphere/auth/configurations/SecurityConfig.java
@@ -36,10 +36,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
+        configuration.setAllowCredentials(false);
         configuration.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
